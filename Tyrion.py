292c9d6b2c7162e1e2a836850c8032764f83b109
maplejune <maplejune@live.co.kr>
2014-02-22 17:32:14 +0900

    Find Imax movie

--- Tyrion.py
+++ Tyrion.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from bs4 import BeautifulSoup
 
 data = {'theaterCd':'0074', 'playYMD':'20140224'}
 response = requests.post('http://m.cgv.co.kr/Schedule/cont/ajaxMovieSchedule.aspx', data)
+soup = BeautifulSoup(response.text)
+timelists = soup.find_all("ul", "timelist")
 
-print response.text
+def isImaxMovieTimelist(timelist):
+	return str(timelist).find('아이맥스') != -1
+
+for timelist in timelists:
+	if isImaxMovieTimelist(timelist):
+		print timelist
