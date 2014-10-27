0272605d7c4754401620ad44175f9139f906faba
maplejune <maplejune@live.co.kr>
2014-10-27 08:29:31 +0000

    Twitter module added

--- init.py
+++ init.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, sqlite3
+import os, sqlite3, json
 
 DB_FILE = os.path.join(os.path.dirname(__file__), 'TICKET.db')
+API_FILE = os.path.join(os.path.dirname(__file__), 'API.txt')
 
 if __name__ == "__main__":
     try:
@@ -12,7 +13,36 @@ if __name__ == "__main__":
         pass
     
     conn = sqlite3.connect(DB_FILE)
+
+    conn.execute('''CREATE TABLE theater
+       (appKey        TEXT     NOT NULL,
+       appSecret      TEXT    NOT NULL,
+       oauthKey       TEXT    NOT NULL,
+       oauthSecret    TEXT    NOT NULL,
+       theaterName    TEXT    NOT NULL,
+       theaterCd      TEXT    NOT NULL);''')
+
+    conn.execute('''CREATE INDEX theaterInfoIndex
+        on theater (theaterCd);''')
     
+    apiInfoList = json.load(open(API_FILE))
+    
+    for apiInfo in apiInfoList:
+        conn.execute('INSERT INTO theater VALUES (?,?,?,?,?,?)', (apiInfo['apiKey'], \
+                                                                    apiInfo['apiSecret'],  \
+                                                                    apiInfo['oauthKey'],  \
+                                                                    apiInfo['oauthSecret'], \
+                                                                    apiInfo['theaterName'], \
+                                                                    apiInfo['theaterCd']))
+    
+    conn.execute('''CREATE TABLE movie
+       (movieIdx        INTEGER     NOT NULL,
+       movieTitle       TEXT    NOT NULL,
+       releaseDate      TEXT    NOT NULL);''')
+
+    conn.execute('''CREATE INDEX movieInfoIndex
+        on movie (movieIdx);''')
+
     conn.execute('''CREATE TABLE ticket
        (theaterCd       TEXT    NOT NULL,
        movieIdx        INTEGER     NOT NULL,
@@ -21,10 +51,14 @@ if __name__ == "__main__":
        insertTime      TEXT    NOT NULL,
        isReported       INTEGER    NOT NULL);''')
 
+    conn.execute('''CREATE INDEX movieIndex
+        on ticket (theaterCd, movieIdx);''')
+    
     conn.execute('''CREATE INDEX ticketIndex
         on ticket (theaterCd, movieIdx, ticketDate);''')
     
     conn.execute('''CREATE INDEX ticketAllIndex
         on ticket (theaterCd, movieIdx, ticketDate, ticketTime);''')
     
+    conn.commit()
     conn.close()
--- reporter.py
+++ reporter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from twython import Twython
 import requests, moment, sqlite3
 import re, os, logging, logging.handlers
 
@@ -15,28 +16,75 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+def getMovieInfo(cursor, movieIdx):
+    cursor.execute('SELECT * FROM movie WHERE movieIdx=?', (movieIdx,))
+    movieInfo = cursor.fetchone()
+    
+    movieTitle = ''
+    movieReleaseDate = ''
+    
+    if movieInfo is None:
+        response = requests.get('http://m.cgv.co.kr/WebApp/Movie/movieDetail.aspx?MovieIdx=%d'%movieIdx)
+
+        movieTitleRaw = re.search(u'<article class="title">(.+)</article>', response.text)
+        if movieTitleRaw:
+            movieTitle = movieTitleRaw.groups()[0]
+
+        movieReleaseDateRaw = re.search(u'<article class="txt1">.*(\d{4}-\d{2}-\d{2}) 개봉</article>', response.text)
+        if movieReleaseDateRaw:
+            movieReleaseDate = movieReleaseDateRaw.groups()[0]
+            
+        if movieTitle != '' and movieReleaseDate != '':
+            cursor.execute('INSERT INTO movie VALUES (?,?,?)', (movieIdx, movieTitle, movieReleaseDate))
+    else:
+        movieTitle = movieInfo[1]
+        movieReleaseDate = movieInfo[2]
+    
+    return {'movieTitle': movieTitle, 'movieReleaseDate': movieReleaseDate}
+
+def sendTweet(apiInfo, messageData):
+    twitter = Twython(apiInfo[0], apiInfo[1], apiInfo[2], apiInfo[3])
+    message = u'%s %s월 %s일 예매가 열렸습니다. %s 예매가능! #%s' % (messageData[0], 
+                                                                   int(messageData[1][4:6]), 
+                                                                   int(messageData[1][6:8]), 
+                                                                   ' '.join([p for p in messageData[2]]),
+                                                                   apiInfo[4])
+    print message
+    #twitter.update_status(status=message)    
+
 if __name__ == "__main__":
     conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
-
-    cursor.execute('SELECT DISTINCT movieIdx FROM ticket WHERE theaterCd=? AND isReported=0', ('0074',))
-    movieIdxRawList = cursor.fetchall()
-
-    for movieIdxRaw in movieIdxRawList:
-        movieIdx = movieIdxRaw[0]
-        
-        cursor.execute('SELECT DISTINCT ticketDate FROM ticket WHERE theaterCd=? AND movieIdx=? AND isReported=0', ('0074', movieIdx,))
-        ticketDateRawList = cursor.fetchall()
-        
-        for ticketDateRaw in ticketDateRawList:
-            ticketDate = ticketDateRaw[0]
-            
-            cursor.execute('SELECT DISTINCT ticketTime FROM ticket WHERE theaterCd=? AND movieIdx=? AND ticketDate=? AND isReported=0', ('0074', movieIdx,ticketDate,))
-            ticketTimeRawList = cursor.fetchall()
-            
-            ticketTimeList = sorted([ticketTimeRaw[0] for ticketTimeRaw in ticketTimeRawList])
-
-            print movieIdx, ticketDate, ticketTimeList
     
+    for theaterCd in ['0074']:
+        cursor.execute('SELECT * FROM theater WHERE theaterCd=?', (theaterCd,))
+        apiInfo = cursor.fetchone()
+    
+        if apiInfo is not None:    
+            cursor.execute('SELECT DISTINCT movieIdx FROM ticket WHERE theaterCd=? AND isReported=0', (theaterCd,))
+            movieIdxRawList = cursor.fetchall()
+
+            for movieIdxRaw in movieIdxRawList:
+                movieIdx = movieIdxRaw[0]
+
+                movieInfo = getMovieInfo(cursor, movieIdx)
+                movieTitle = movieInfo['movieTitle']
+                movieReleaseDate = moment.date(movieInfo['movieReleaseDate'])
+
+                cursor.execute('SELECT DISTINCT ticketDate FROM ticket WHERE theaterCd=? AND movieIdx=? AND isReported=0', (theaterCd, movieIdx,))
+                ticketDateRawList = cursor.fetchall()
+
+                for ticketDateRaw in ticketDateRawList:
+                    ticketDate = ticketDateRaw[0]
+
+                    if moment.date(ticketDate, 'YYYYMD') >= movieReleaseDate:           
+                        cursor.execute('SELECT DISTINCT ticketTime FROM ticket WHERE theaterCd=? AND movieIdx=? AND ticketDate=? AND isReported=0', \
+                                       (theaterCd, movieIdx,ticketDate,))
+                        ticketTimeRawList = cursor.fetchall()
+                        ticketTimeList = sorted([ticketTimeRaw[0] for ticketTimeRaw in ticketTimeRawList])
+
+                        sendTweet(apiInfo, (movieTitle, ticketDate, ticketTimeList))
+
+    conn.commit()
     conn.close()
     
