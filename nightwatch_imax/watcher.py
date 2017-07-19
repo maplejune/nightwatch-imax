78c33ec6c4fa21752646a9a9d81c300308da6de1
maplejune <maplejune@live.co.kr>
2017-07-20 01:00:11 +0900

    wip

--- nightwatch_imax/watcher.py
+++ nightwatch_imax/watcher.py
@@ -2,15 +2,16 @@
 import re
 import requests
 
-schedule_response = requests.get("http://m.cgv.co.kr/Schedule/?tc=0074&t=T&ymd=20170719&src=").text
+date_list_response = requests.get("http://m.cgv.co.kr/Schedule/?tc=0074&t=T&ymd=20170719&src=").text
 
-schedule_pattern = re.compile("var ScheduleDateData = \[(.*)\]", re.MULTILINE)
-schedule = schedule_pattern.search(schedule_response).group(1).encode().decode("unicode-escape")
+date_list_pattern = re.compile("var ScheduleDateData = \[(.*)\]", re.MULTILINE)
+date_list = date_list_pattern.search(date_list_response).group(1).encode().decode("unicode-escape")
 
 date_pattern = re.compile("getMovieSchedule\('(\d{8})',")
-dates = date_pattern.findall(schedule)
-
-date_response = requests.post("http://m.cgv.co.kr/Schedule/cont/ajaxMovieSchedule.aspx", data={"theaterCd": "0074", "playYMD": "20170720"})
+dates = date_pattern.findall(date_list)
 
 print(dates)
-print(date_response.text)
+
+schedule_response = requests.post("http://m.cgv.co.kr/Schedule/cont/ajaxMovieSchedule.aspx", data={"theaterCd": "0074", "playYMD": "20170720"})
+
+print(schedule_response.text)
