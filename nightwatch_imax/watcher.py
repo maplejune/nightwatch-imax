308bbb3ac1dd8cbc3d2fb1c047dcd182ceec4b44
maplejune <planetes853@gmail.com>
2017-07-25 00:42:03 +0900

    Optimization for AWS Lambda

--- nightwatch_imax/watcher.py
+++ nightwatch_imax/watcher.py
@@ -1,14 +1,35 @@
 # -*- coding: utf-8 -*-
 import logging
-import re
 import os
+import re
 
-import boto3
 import arrow
+import boto3
 import requests
 from bs4 import BeautifulSoup
 
 MOVIE_CODE_PATTERN = re.compile("popupSchedule\('.*','.*','(\d\d:\d\d)','\d*','\d*', '(\d*)', '\d*', '\d*',")
+
+
+class ScheduleInfo:
+    id = ''
+
+    def __init__(self, theater_code, date, raw_data):
+        self.theater_code = theater_code
+        self.date = date
+        self.raw_data = raw_data
+
+        movie_info = MOVIE_CODE_PATTERN.search(raw_data)
+
+        if movie_info is None:
+            logger.warning('Wrong schedule_info : %s', raw_data)
+        else:
+            self.time = movie_info.group(1).replace(':', '')
+            self.movie_code = movie_info.group(2)
+            self.id = '{}.{}.{}.{}'.format(theater_code, date, self.movie_code, self.time)
+
+    def is_valid(self):
+        return self.id is not ''
 
 
 def is_cgv_online():
@@ -32,53 +53,45 @@ def get_date_list(theater_code):
     date_pattern = re.compile('getMovieSchedule\(\'(\d{8})\',')
     dates = date_pattern.findall(date_list)
 
+    logger.info('Targets : %s %s', theater_code, dates)
+
     return dates
 
 
-def get_schedule_list(theater_code, date):
-    schedule_url = 'http://m.cgv.co.kr/Schedule/cont/ajaxMovieSchedule.aspx'
-    schedule_response = requests.post(schedule_url, data={'theaterCd': theater_code, 'playYMD': date}).text
-    soup = BeautifulSoup(schedule_response, 'html.parser')
+def get_schedule_list(theater_code):
+    result = []
 
-    schedule_list = []
+    for date in get_date_list(theater_code):
+        logger.info('Target : %s %s', theater_code, date)
 
-    for time_list in soup.find_all('ul', 'timelist'):
-        schedule_list.extend(time_list.find_all('li'))
+        schedule_url = 'http://m.cgv.co.kr/Schedule/cont/ajaxMovieSchedule.aspx'
+        schedule_response = requests.post(schedule_url, data={'theaterCd': theater_code, 'playYMD': date}).text
+        soup = BeautifulSoup(schedule_response, 'html.parser')
 
-    return schedule_list
+        schedule_list = []
+        for time_list in soup.find_all('ul', 'timelist'):
+            schedule_list.extend(time_list.find_all('li'))
+
+        result.extend([ScheduleInfo(theater_code, date, str(raw_data)) for raw_data in schedule_list])
+
+    return list(filter(lambda schedule_info: schedule_info.is_valid(), result))
 
 
-def get_schedule_info(schedule_str):
-    return MOVIE_CODE_PATTERN.search(schedule_str)
-
-
-def save(theater_code, date, schedule_list):
+def save(schedule_list):
     created_at = arrow.utcnow().timestamp
+    table = boto3.resource('dynamodb').Table('nightwatch-imax-raw-data')
 
     with table.batch_writer(overwrite_by_pkeys=['id', 'created_at']) as batch:
-        for schedule in schedule_list:
-            raw_data = str(schedule)
-
-            schedule_info = get_schedule_info(raw_data)
-
-            if schedule_info is None:
-                logger.warning('Missing schedule_info : %s', raw_data)
-                continue
-
-            time = schedule_info.group(1).replace(':', '')
-            movie_code = schedule_info.group(2)
-
-            raw_data_id = '{}.{}.{}.{}'.format(theater_code, date, movie_code, time)
-
-            batch.put_item(Item={'id': raw_data_id,
+        for schedule_info in schedule_list:
+            batch.put_item(Item={'id': schedule_info.id,
                                  'created_at': created_at,
-                                 'raw_data': raw_data,
-                                 'theater_code': theater_code,
-                                 'date': date,
-                                 'movie_code': movie_code,
-                                 'time': time})
+                                 'raw_data': schedule_info.raw_data,
+                                 'theater_code': schedule_info.theater_code,
+                                 'date': schedule_info.date,
+                                 'movie_code': schedule_info.movie_code,
+                                 'time': schedule_info.time})
 
-            logger.info('Saved : %s', raw_data_id)
+            logger.debug('Saved : %s', schedule_info.id)
 
 
 def watcher_lambda_handler(event, context):
@@ -86,14 +99,11 @@ def watcher_lambda_handler(event, context):
         raise Exception('Cannot connect CGV server!')
 
     theater_code = os.environ['theater_code']
-
-    for date in get_date_list(theater_code):
-        schedule_list = get_schedule_list(theater_code, date)
-
-        save(theater_code, date, schedule_list)
+    schedule_list = get_schedule_list(theater_code)
+    save(schedule_list)
 
     return theater_code
 
 
 logger = logging.getLogger()
-table = boto3.resource('dynamodb').Table('nightwatch-imax-raw-data')
+logger.setLevel(logging.INFO)
